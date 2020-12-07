0d139b1615695818b5b026ba06917d3c3c1b1136
Mikhail <gormih78@yandex.ru>
2020-12-07 10:48:19 +0300

    add setup.py and requriments.txt files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='daisy_online_client',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/gormih/daisy_online_client',
+    license='MIT',
+    author='Mikhail Goryunov',
+    author_email='gormih78@yandex.ru',
+    description='Library for Daisy Online client on Python 3.6+'
+)
